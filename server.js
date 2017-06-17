cba85e0cd9ae804d143c735cfa6a2164594b4ceb
g1st <gintstan@gmail.com>
2017-06-17 11:05:33 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.set('trust proxy', 'loopback');
+
+app.get('/', function(req, res) {
+  // const IP = req.headers['x-forwarded-for']
+  console.log(req.headers);
+  const IP = req.ip;
+  res.send(IP);
+});
+
+app.listen(PORT, function() {
+  console.log(`app is listening on port ${PORT}`);
+});
