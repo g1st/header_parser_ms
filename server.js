f9a4c0233448cd17d822714fd7bde105a0b4a150
g1st <gintstan@gmail.com>
2017-06-17 11:47:18 +0100

    ip

--- server.js
+++ server.js
@@ -3,13 +3,24 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+
 app.get('/', function(req, res) {
-  const IP = req.headers['x-forwarded-for'];
-  // var IP = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  console.log(IP);
-  res.send(req.headers);
+  const header = headerParser(req.headers);
+  console.log(header);
+  res.send(header);
 });
 
 app.listen(PORT, function() {
   console.log(`app is listening on port ${PORT}`);
 });
+
+function headerParser(header) {
+  const header_skeleton = {
+    ipaddress: null,
+    language: null,
+    software: null
+  };
+
+  header_skeleton.ipaddress = header["x-forwarded-for"];
+  return header_skeleton;
+}
